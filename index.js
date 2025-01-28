8542032df7cdf2a583d0ce5a10b45d46b786d5cd
Aniket Raut <rautnaniket@gmail.com>
2025-01-28 23:45:36 +0530

    Initiated Express Server and Connected DB

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on  http://localhost:${PORT}`);
+});
