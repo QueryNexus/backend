178160b83b5cbb7451f9b54c11b91eae599d1c4b
Aniket Raut <rautnaniket@gmail.com>
2025-03-25 20:17:44 +0530

    Refactor server setup: remove serverless-http, simplify API response

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
-const serverless = require('serverless-http');
-const cors = require('cors');
 const userRouter = require("./routes/userRoutes");
 const companyRouter = require("./routes/companyRouter");
 const queriesRouter = require("./routes/queriesRouter");
 
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
 connectDB();
-
-
 app.use(express.json());
 
 app.use(userRouter);
 app.use(companyRouter);
 app.use(queriesRouter);
 
-
 app.get("/", async (req, res) => {
-    res.send("API is running on AWS...");
+  res.send("API is running...");
 });
 
-module.exports.handler = serverless(app);
+module.exports = app;
